cd62ca906585273986899f8b9ba9fc04910aaec1
Edu Botero <banocx@gmail.com>
2023-12-12 00:01:09 -0500

    added scrip for Network input and script for player input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,93 +5,92 @@ using Fusion;
 using Fusion.Sockets;
 using System;
 
-public class PlayerSpawner : MonoBehaviour, INetworkRunnerCallbacks
+public class PlayerInput : MonoBehaviour, INetworkRunnerCallbacks
 {
-    [SerializeField]
-    private NetworkPrefabRef _prayerPrefab;
     public void OnConnectedToServer(NetworkRunner runner)
     {
-       
+        //throw new NotImplementedException();
     }
 
     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
     {
-       
+        //throw new NotImplementedException();
     }
 
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
     {
-        
+       // throw new NotImplementedException();
     }
 
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnDisconnectedFromServer(NetworkRunner runner)
     {
-        
+       // throw new NotImplementedException();
     }
 
     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
     {
-        
+       // throw new NotImplementedException();
     }
 
+    #region RunnerCallbacks
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
-       
+        PlayerInputData inputData = new PlayerInputData();
+
+        inputData.Direction = Vector3.forward;
+
+        input.Set(inputData);
     }
 
+    #endregion
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
     {
-       
+        //throw new NotImplementedException();
     }
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        if (player == runner.LocalPlayer) 
-        {
-            Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(1, 5), 0.5f, UnityEngine.Random.Range(1, 5));
-
-            runner.Spawn(_prayerPrefab, spawnPosition, Quaternion.identity, player);
-        }
+        //throw new NotImplementedException();
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
     {
-       
+        //throw new NotImplementedException();
     }
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnSceneLoadStart(NetworkRunner runner)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
     {
-        
+        //throw new NotImplementedException();
     }
 
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
     {
-        
+        //throw new NotImplementedException();
     }
 
     // Start is called before the first frame update
@@ -105,4 +104,9 @@ public class PlayerSpawner : MonoBehaviour, INetworkRunnerCallbacks
     {
         
     }
+}
+
+public struct PlayerInputData: INetworkInput
+{
+    public Vector3 Direction;
 }
